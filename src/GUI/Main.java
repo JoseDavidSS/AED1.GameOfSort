1907efad8a7408229213a86c22b480bd2998fb8b
JoseDavidSS <39009363+JoseDavidSS@users.noreply.github.com>
2018-10-09 18:36:29 -0600

    Se crea la plantilla para el menú, se crea la clase Main para ejecutar la interfaz, y se crea la clase Game junto al archivo .fxml para ser usado con Scene Builder.

---
+++ src/GUI/Main.java
@@ -0,0 +1,55 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+/**
+ * Clase para iniciar la interfaz.
+ */
+
+public class Main extends Application {
+
+    static Stage game;
+
+    /**
+     * @param scene_route es un argumento que entra al momento de ser llamado por el controller Juego y así definir cuál ventana mostrar.
+     * @throws IOException se usa para verifica cuando hay errores
+     */
+
+    static void setScene(String scene_route) throws IOException {
+        Parent root = FXMLLoader.load(Main.class.getResource(scene_route));
+        Scene scene = new Scene(root);
+        double width = game.getWidth();
+        double height = game.getHeight();
+        boolean fullscreen = game.isFullScreen();
+        game.setScene(scene);
+
+    }
+
+    /**
+     * main es el método que ejecuta la clase una vez que se inicializa.
+     * @param args argumentos que son necesarios para inicializar y correr la clase.
+     */
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+
+    /**
+     * El método start se utiliza para definir con qué ventana se iniciará la interfaz.
+     */
+
+    public void start(Stage primaryStage) throws Exception{
+        Main.game=primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("juegoFXML.fxml"));
+        primaryStage.setTitle("¡¡ Game of Sorts !!");
+        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.show();
+    }
+}
