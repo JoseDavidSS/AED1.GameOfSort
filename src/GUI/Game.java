5687b03a2848fb28f8a8a925a08df80e2a97237e
JoseDavidSS <39009363+JoseDavidSS@users.noreply.github.com>
2018-10-09 20:26:02 -0600

    Se elabora un menú básico y una pantalla secundaria de juego.

--- src/GUI/Game.java
+++ src/GUI/Game.java
@@ -1,4 +1,17 @@
 package GUI;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class Game {
+    @FXML
+
+    /**
+     * Método que cambia la escena gracias a una instancia que llama del Main.
+     */
+    public void runBoard () throws IOException {
+        Main.setScene("Board.fxml");
+    }
+
 }
