ab5bc935cfc55c9a7217346eec1e1720c925d56e
Apu <jp.1398@hotmail.com>
2018-10-16 00:20:16 -0600

    Añade funcionalidad al servlet

--- src/Server/Sort.java
+++ src/Server/Sort.java
@@ -1,14 +1,16 @@
 package Server;
 
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-
+@WebServlet(name = "Sort",urlPatterns = "sort")
 public class Sort extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        //String contenido = request.getParameter("Contenido");
+        response.getWriter().println("Hola");
     }
 }
