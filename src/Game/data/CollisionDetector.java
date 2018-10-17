e939e0ccb8e5984a4b0aae8a973229057151d602
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2018-10-17 10:07:06 -0600

    El jugador muere si recibe tres impactos

--- src/Game/data/CollisionDetector.java
+++ src/Game/data/CollisionDetector.java
@@ -27,7 +27,7 @@ public class CollisionDetector extends Thread {
                         TemporalNode sub_temp2 = tmp2.head;
                         while (i != Game.onScreenEnemies){
                             Dragon sub_sub_tmp2 = sub_temp2.getDragon();
-                            if(sub_sub_tmp.getBoundsInParent().intersects(sub_sub_tmp2.getBoundsInParent())){
+                            if (sub_sub_tmp.getBoundsInParent().intersects(sub_sub_tmp2.getBoundsInParent())){
                                 sub_sub_tmp.setDead(true);
                                 sub_sub_tmp2.hit();
                                 this.sleep();
@@ -40,7 +40,14 @@ public class CollisionDetector extends Thread {
                         sub_tmp = sub_tmp.next;
                     }
                     else{
-                        sub_tmp = sub_tmp.next;
+                        if (sub_sub_tmp.getBoundsInParent().intersects((Game.player.getBoundsInParent()))){
+                            sub_sub_tmp.setDead(true);
+                            Game.player.hit();
+                            this.sleep();
+                            break;
+                        }else{
+                            sub_tmp = sub_tmp.next;
+                        }
                     }
                 }
             }
